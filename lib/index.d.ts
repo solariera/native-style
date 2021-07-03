c3128721fe0c742856bde5d5824c616cac90541c
Always <alwaysrusting@gmail.com>
2021-07-04 02:08:10 +0900

    000017 [fix] bug fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,4 @@ export { imagePositionStyle, imageSizeStyle, imageViewStyle } from './image';
 export { blurStyle, borderStyle, colorStyle, marginStyle, opacityStyle } from './object';
 export { paddingStyle, positionStyle, roundStyle, sizeStyle } from './object';
 export { fontColorStyle, fontFamilyStyle, fontSizeStyle } from './text';
-export { placementStyle, letterSpacingStyle, strokeStyle } from './text';
+export { placementStyle, letterSpacingStyle, textStrokeStyle } from './text';
--- lib/text/strokeStyle/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type StrokeType = {
-    strokeWidth?: number;
-    strokeColor?: string;
-    strokeColorAlpha?: number;
-};
-declare const strokeStyle: (props: StrokeType) => import("styled-components").FlattenSimpleInterpolation;
-export { strokeStyle };
